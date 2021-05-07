ab4bbdff67fb67d6477229e30fd011e338daf2a1
Mohammedamin Abdullah <59562112+mohammedaminA@users.noreply.github.com>
2021-05-07 23:45:31 +0300

    Create weather.dart

---
+++ lib/services/weather.dart
@@ -0,0 +1,52 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const token = '21249ce59d1b608ad144043305ae2e2d';
+
+class WeatherModel {
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+
+
+
+    NetworkHelper network = NetworkHelper(url: 'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$token&units=metric');
+    var weatherData = await network.getData();
+    return weatherData;
+  }
+
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
