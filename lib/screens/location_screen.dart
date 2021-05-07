13c0e0836b030da6c67bda483ac1cbd2394c5803
Mohammedamin Abdullah <59562112+mohammedaminA@users.noreply.github.com>
2021-05-07 16:59:35 +0300

    Update location_screen.dart

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 
 class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+  final locationWeather;
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
