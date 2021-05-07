82148605dd09b33cd7b1e42f96951e1f19d1ab14
Mohammedamin Abdullah <59562112+mohammedaminA@users.noreply.github.com>
2021-05-08 02:30:23 +0300

    Update weather.dart

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -4,7 +4,13 @@ import 'package:clima/services/networking.dart';
 const token = '21249ce59d1b608ad144043305ae2e2d';
 
 class WeatherModel {
-
+  
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper network = NetworkHelper(url: 'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$token&units=metric');
+    var weatherData = await network.getData();
+    return weatherData;
+  }
+  
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
